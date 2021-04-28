8e99efdc40febbfe300f7a20bd29dc0cf7b6c09b
antonioarieiro <sfgfffs@gmail.com>
2021-04-28 20:24:38 -0300

    project wallet end front-end

---
+++ src/pages/Login.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { userLoginAction } from '../actions';
+import './Login.css'
+
+class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isButtonDisabled: true,
+      shouldRedirect: false,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.verifyFieldsValidation = this.verifyFieldsValidation.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  onInputChange({ target }) {
+    this.setState(
+      { [target.name]: target.value },
+      () => {
+        this.setState({ isButtonDisabled: !this.verifyFieldsValidation() });
+      },
+    );
+  }
+
+  onLogin(email) {
+    const { submitLogin } = this.props;
+    submitLogin(email);
+
+    this.setState({ shouldRedirect: true });
+  }
+
+  verifyFieldsValidation() {
+    const { email, password } = this.state;
+    const MINIMAL_PASSWORD_CHARS = 6;
+    const emailRegex = new RegExp([
+      '^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)',
+      '|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])',
+      '|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
+    ].join(''));
+
+    const isEmailValid = emailRegex.test(email);
+    const isPasswordValid = password.length >= MINIMAL_PASSWORD_CHARS;
+
+    return isEmailValid && isPasswordValid;
+  }
+
+  render() {
+    const { email, password, isButtonDisabled, shouldRedirect } = this.state;
+
+    if (shouldRedirect) return <Redirect to="/carteira" />;
+
+    return (
+      <>
+        <form className="dale">
+          <fieldset>
+              <input
+                type="text"
+                placeholder="Digite seu email ex: test@test.com"
+                name="email"
+                data-testid="email-input"
+                onChange={ this.onInputChange }
+                value={ email }
+              />
+              {
+                isButtonDisabled ?
+                `digite o Email no Formato Padrao ex: 'test@test.com'`
+                :''
+              }
+              <input
+                type="password"
+                placeholder="Senha"
+                name="password"
+                data-testid="password-input"
+                onChange={ this.onInputChange }
+                value={ password }
+              />
+              {
+                isButtonDisabled === false ?
+                <button
+                type="button"
+                disabled={ isButtonDisabled }
+                onClick={ () => this.onLogin(email) }
+              >
+                Entrar
+              </button>
+              : ''
+              }
+            </fieldset>
+        </form>
+      </>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  submitLogin: (email) => dispatch(userLoginAction(email)),
+});
+
+Login.propTypes = {
+  submitLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
---
+++ src/reducers/user.js
@@ -0,0 +1,16 @@
+import { USER_LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case USER_LOGIN:
+    return { ...state, email: action.email };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
